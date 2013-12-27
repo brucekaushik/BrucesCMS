57f3b1491d34762ec5c41547a5a8c5fc1aa38462
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 14:13:09 +0530

    changed logout link path

--- includes/topnav.php
+++ includes/topnav.php
@@ -3,5 +3,5 @@
 	<a href="home.php?action=create-page">Create Page</a> |
 	<a href="home.php?action=edit-pages-list">View or Edit Pages</a> | 
 	<a href="home.php?action=delete-pages-list">Delete Pages</a> | 
-	<a href="../08-adminArea/home.php?action=logout">Logout</a>
-</div><br><br>
+	<a href="../BrucesAdminArea/home.php?action=logout">Logout</a>
+</div>
